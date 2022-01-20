a07541b4ae3dde1f5f766a56308f61f250890950
Isaac Raskin <yitzchokr@live.com>
2022-01-20 14:00:41 +0000

    add config files for plesk

--- config/database.js
+++ config/database.js
@@ -1,11 +1,30 @@
 const path = require('path');
 
 module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
-    connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+  defaultConnection: env('DATABASE_CONNECTION_NAME'), 
+  connections: {
+    sqlite: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'sqlite',
+        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+      },
+      options: {
+        useNullAsDefault: true,
+      },
     },
-    useNullAsDefault: true,
+    mysql: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'mysql',
+        host: env('DATABASE_HOST'),
+        port: env.int('DATABASE_PORT'),
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
+        ssl: env.bool('DATABASE_SSL', false),
+      },
+      options: {}
+    }
   },
 });
