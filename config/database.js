2e89c794217885e4638d126a3d47079ad3d79ff6
Isaac Raskin <yitzchokr@live.com>
2022-02-14 13:27:49 +0000

    add docker file

--- config/database.js
+++ config/database.js
@@ -1,10 +1,13 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: env("DATABASE_CLIENT", "postgres"),
     connection: {
-      filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "strapi"),
+      user: env("DATABASE_USERNAME", "strapi"),
+      password: env("DATABASE_PASSWORD", "strapi"),
     },
-    useNullAsDefault: true,
     debug: false,
   },
 });
